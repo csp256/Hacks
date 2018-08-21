69bb614685ff092d0b036e3028895deda9391330
Christopher Parker <csp256@users.noreply.github.com>
2018-08-21 11:17:10 -0700

    Update Opaque_Typedef.h

--- src/Opaque_Typedef.h
+++ src/Opaque_Typedef.h
@@ -1,7 +1,9 @@
 
-struct Foo {};
-typedef Foo Bar;
-std::is_same<Foo, Bar>::value; // true
+{
+    struct Foo {};
+    typedef Foo Bar;
+    std::is_same<Foo, Bar>::value; // true
+}
 
 /*
     What if we wanted to create a new, different type which was elsewise-identical to Foo?
@@ -11,32 +13,34 @@ std::is_same<Foo, Bar>::value; // true
     A workaround is available using Tagged Dispatch. 
 */
 
-template <typename T, typename Tag>
-struct Foo {
-    T data;
-    void Name() { return Tag::Name(); }
-}
+{
+    template <typename T, typename Tag>
+    struct Foo {
+        T data;
+        void Name() { return Tag::Name(); }
+    }
 
-#define STR(X) #X
-#define OPAQUE_TYPEDEF(EXTANT_TYPE, NEW_TYPE, ARG_1) \
-    struct NEW_TYPE##_UNIQUE_TAG { \
-        static constexpr char Name[] = STR(NAME); \
-    }; \
-    typedef EXTANT_TYPE##<ARG_1, NEW_TYPE##_UNIQUE_TAG> ;
+    #define STR(X) #X
+    #define OPAQUE_TYPEDEF(EXTANT_TYPE, NEW_TYPE, ARG_1) \
+        struct NEW_TYPE##_UNIQUE_TAG { \
+            static constexpr char Name[] = STR(NAME); \
+        }; \
+        typedef EXTANT_TYPE##<ARG_1, NEW_TYPE##_UNIQUE_TAG>
 
-OPAQUE_TYPEDEF(Foo, Bar1, double);
-OPAQUE_TYPEDEF(Foo, Bar2, double);
+    OPAQUE_TYPEDEF(Foo, Bar1, double);
+    OPAQUE_TYPEDEF(Foo, Bar2, double);
 
-#undef OPAQUE_TYPEDEF
-#undef STR
+    #undef OPAQUE_TYPEDEF
+    #undef STR
 
-std::is_same<Foo, Bar1>::value; // false
-std::is_same<Foo, Bar2>::value; // false
-std::is_same<Bar1, Bar2>::value; // false
+    std::is_same<Foo, Bar1>::value; // false
+    std::is_same<Foo, Bar2>::value; // false
+    std::is_same<Bar1, Bar2>::value; // false
 
 /*
     Note this also provides easy reflection:
 */
 
-std::cout << Bar1.Name() << std::endl; // Bar1
-std::cout << Bar2.Name() << std::endl; // Bar1
+    std::cout << Bar1.Name() << std::endl; // Bar1
+    std::cout << Bar2.Name() << std::endl; // Bar2
+}

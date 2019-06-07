a4b03f448b59c229c8063a9dc2680b5d1dff3054
Christopher Parker <csp256@users.noreply.github.com>
2019-06-07 14:45:49 -0700

    Update ENABLE_IF_SAME.h

--- src/ENABLE_IF_SAME.h
+++ src/ENABLE_IF_SAME.h
@@ -1,3 +1,79 @@
+/*
+
+    Enables a function only if each of a variadic number of types (or each of a tuple)
+    is contained within a whitelist (stored as a tuple of types).
+
+*/
+
+#include <type_traits>
+#include <tuple>
+
+using whitelist = std::tuple<int, bool>;
+
+//
+
+template <typename T, class Tuple>
+struct is_any;
+
+template <class T, class ... U>
+struct is_any<T, std::tuple<U ...>> 
+    : std::disjunction< std::is_same<T, U> ... > {};
+
+//
+
+template <class T, class U>
+struct all_in_whitelist;
+
+template <class ... T, class U>
+struct all_in_whitelist<std::tuple<T ...>, U> 
+    : std::conjunction< is_any<T, U> ... > {};
+
+//
+
+template <typename T>
+using enable_if_ = std::enable_if_t< T::value >;
+
+template<class U, class ... T >
+using enable_if_whitelisted = enable_if_< all_in_whitelist<std::tuple<T ...>, U> >;
+
+//
+
+#define UNPACK( ... ) __VA_ARGS__
+
+#define ENABLE_IF_IMPL( ARG ) typename = enable_if_whitelisted< UNPACK ARG > 
+
+#define ENABLE_IF( ... ) ENABLE_IF_IMPL((__VA_ARGS__))
+
+//
+
+template <typename ... T, ENABLE_IF(whitelist, T ...)>
+void f(std::tuple<T ...> t)
+{}
+
+template <typename T, ENABLE_IF(whitelist, T)>
+void g(T  t)
+{}
+
+//
+
+int main()
+{
+    auto x = std::tuple{1, true};
+    f(x);
+
+    auto y = std::tuple{1, true, 3.14};
+    // f(y);
+
+    g(3);
+    // g(3.14);
+
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
 #include <type_traits>
 
 template <class T, class ... Args>
